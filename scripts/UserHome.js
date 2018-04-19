a5b7b00af1f194003ea9c4aa217baa8772c693dd
smokwid20 <taikhoangchung9196@gmail.com>
2018-04-19 23:10:31 +0700

    added owl carousel js

--- scripts/UserHome.js
+++ scripts/UserHome.js
@@ -10,3 +10,26 @@ window.onscroll = function hideNav() {
   }
   prevScrollPos = currentScrollPos;
 };
+
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true
+      },
+      600: {
+        items: 3,
+        nav: false
+      },
+      1000: {
+        items: 5,
+        nav: true,
+        loop: false
+      }
+    }
+  });
+});
