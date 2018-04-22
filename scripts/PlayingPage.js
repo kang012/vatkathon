5f4a12fe870e07fd2e80f79538b66b1445c403bf
mihawk <mihawk@mihawks-iMac.local>
2018-04-22 23:41:54 +0700

    working with playing page

--- scripts/PlayingPage.js
+++ scripts/PlayingPage.js
@@ -1,22 +1,35 @@
+var prevScrollPos = window.pageYOffset;
+var bigNav = document.getElementById('big-nav');
+/* scrolling increasing pageY value so if it higher than default(1) hide navbar   */
+window.onscroll = function hideNav() {
+  var currentScrollPos = window.pageYOffset;
+  if (currentScrollPos > prevScrollPos) {
+    bigNav.style.top = '-50px';
+  } else {
+    bigNav.style.top = '0';
+  }
+  prevScrollPos = currentScrollPos;
+};
+
 $(document).ready(function () {
-    $('.owl-carousel').owlCarousel({
-      loop: true,
-      margin: 10,
-      responsiveClass: true,
-      responsive: {
-        0: {
-          items: 1,
-          nav: true
-        },
-        600: {
-          items: 3,
-          nav: false
-        },
-        1000: {
-          items: 5,
-          nav: true,
-          loop: false
-        }
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true
+      },
+      600: {
+        items: 3,
+        nav: false
+      },
+      1000: {
+        items: 5,
+        nav: true,
+        loop: false
       }
-    });
+    }
   });
+});
