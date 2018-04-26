692a15f025064cea9b6ca266fdee322fd81e14c0
mihawk <mihawk@mihawks-iMac.local>
2018-04-26 23:11:46 +0700

    volumebar is now working

--- scripts/PlayingPage.js
+++ scripts/PlayingPage.js
@@ -6,14 +6,15 @@ var currentTime = document.getElementById('current-time');
 var songDuration = document.getElementById('song-duration');
 var playButton = document.getElementById('play-button');
 var progressMusic = document.getElementById('progress-music');
-var currentProgress = document.getElementById('progress-current-music');
+var progressCurrent = document.getElementById('progress-current-music');
 var expandLyric = document.getElementById('expand-lyric');
 var lyricContent = document.getElementById('lyric-content');
-var volumePercent = document.getElementById('volume-percent');
-var volumeBar = document.getElementById('volumn-bar');
-var volumeSlider = document.getElementById('volume-slider');
+var currentVolume = document.getElementById('current-volume');
+var volumeBar = document.getElementById('volume-bar');
+var volumeSlider = document.getElementById('slider-volume');
 var progressMusicWidth = progressMusic.offsetWidth;
 var onHold = false;
+var onVolumeHold = false;
 var lyricNotExpended = true;
 $(document).ready(function () {
   $('.owl-carousel').owlCarousel({
@@ -60,7 +61,7 @@ function secondToMin(inputTime) {
 // sync slider with current music time
 function timeUpdate() {
   var playTime = progressMusicWidth * (music.currentTime / music.duration);
-  currentProgress.style.width = ((playTime - slider.offsetWidth)) + 'px';
+  slider.style.left = ((playTime - slider.offsetWidth)) + 'px';
   currentTime.innerHTML = secondToMin(music.currentTime);
   songDuration.innerHTML = secondToMin(music.duration);
   if (music.ended) {
@@ -76,24 +77,53 @@ function clickPlay() {
     playButton.innerHTML = '<i class="fas fa-play"></i>';
   }
 }
+
+/* Reuseable function */
 function getPosition(element) {
   return element.getBoundingClientRect().left;
 }
 // get width based on position of mouse click in a div
-function clickPercent(event) {
-  return (event.clientX - getPosition(progressMusic)) / progressMusicWidth;
+function clickPercent(event, timeLine, timeLineLength) {
+  return (event.clientX - getPosition(timeLine)) / timeLineLength;
 }
 function moveSlider(event) {
   // get width base on mouse coordinate
-  var newPos = ((event.clientX - getPosition(progressMusic)) - slider.offsetWidth) / 2;
+  var newPos = event.clientX - getPosition(progressMusic);
   if (newPos > 0 && newPos < progressMusic.offsetWidth) {
-    currentProgress.style.width = newPos + 'px';
+    progressCurrent.style.width = newPos + 'px';
   } else if (newPos < 0) {
-    currentProgress.style.width = 0;
+    progressCurrent.style.width = 0;
   } else if (newPos >= progressMusic.offsetWidth) {
-    currentProgress.style.width = 100 + '%';
+    progressCurrent.style.width = 100 + '%';
   }
 }
+
+function moveVolumeSlider(event) {
+  // get width base on mouse coordinate
+  var newPos = event.clientX - getPosition(volumeBar);
+  if (newPos > 0 && newPos < volumeBar.offsetWidth) {
+    currentVolume.style.width = newPos + 'px';
+  } else if (newPos < 0) {
+    currentVolume.style.width = 0;
+  } else if (newPos >= volumeBar.offsetWidth) {
+    currentVolume.style.width = 100 + '%';
+  }
+}
+
+function mouseDownVolume() {
+  onVolumeHold = true;
+  window.addEventListener('mousemove', moveVolumeSlider);
+}
+function mouseUpVolume(event) {
+  if (onVolumeHold === true) {
+    moveVolumeSlider(event);
+    window.removeEventListener('mousemove', moveVolumeSlider);
+    music.volume = 1.0 * clickPercent(event, volumeBar, volumeBar.offsetWidth);
+  }
+  onVolumeHold = false;
+}
+
+
 function mouseDown() {
   onHold = true;
   window.addEventListener('mousemove', moveSlider);
@@ -103,11 +133,13 @@ function mouseUp(event) {
   if (onHold === true) {
     moveSlider(event);
     window.removeEventListener('mousemove', moveSlider);
-    music.currentTime = music.duration * clickPercent(event);
+    music.currentTime = music.duration * clickPercent(event, progressMusic, progressMusicWidth);
     music.addEventListener('timeupdate', timeUpdate);
   }
   onHold = false;
 }
+
+// Toggle expand or narrow lyric section
 function viewMoreLyric() {
   if (lyricNotExpended) {
     lyricContent.style.height = 'auto';
@@ -128,11 +160,22 @@ music.addEventListener('timeupdate', timeUpdate);
 // Make progressbar clickable
 progressMusic.addEventListener('click', function changeMusicDuration(event) {
   moveSlider(event);
-  music.currentTime = music.duration * clickPercent(event);
+  music.currentTime = music.duration * clickPercent(event, progressMusic, progressMusicWidth);
 });
 // Make slider draggable
 slider.addEventListener('mousedown', mouseDown);
 window.addEventListener('mouseup', mouseUp);
 // Expand or narrow lyric section
 expandLyric.addEventListener('click', viewMoreLyric);
-// make volumn draggable
+
+// make volume clickable
+volumeBar.addEventListener('click', function changeVolume(event) {
+  moveVolumeSlider(event);
+  music.volume = 1.0 * clickPercent(event, volumeBar, volumeBar.offsetWidth);
+});
+// make volume draggable
+volumeSlider.addEventListener('mousedown', mouseDownVolume);
+window.addEventListener('mouseup', mouseUpVolume);
+/* how to get new value base on clicked position
+  newValue = (currentLength/fullLength) * highestValue;
+*/
